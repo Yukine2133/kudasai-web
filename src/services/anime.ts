f92365379dabc2c37f4f7e1476cae12da11cc2fe
Yukine213 <kondratenkov1109@gmail.com>
2023-07-03 18:40:03 +0300

    add getAnimeById

--- src/services/anime.ts
+++ src/services/anime.ts
@@ -3,11 +3,11 @@ import { Data, RootData } from "../interface/interface";
 
 export const animeApi = createApi({
   reducerPath: "animeApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://api.jikan.moe/v4/top/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.jikan.moe/v4/" }),
   endpoints: (builder) => ({
     getAnime: builder.query<Data[], { filter: string; page: number }>({
       query: ({ filter, page }) => ({
-        url: `anime`,
+        url: `top/anime`,
         params: {
           filter: filter,
           page: page.toString(),
@@ -15,7 +15,13 @@ export const animeApi = createApi({
       }),
       transformResponse: (response: RootData<Data>) => response.data,
     }),
+    getAnimeById: builder.query<Data[], { id: number }>({
+      query: ({ id }) => ({
+        url: `anime/${id}`,
+      }),
+      transformResponse: (response: RootData<Data>) => response.data,
+    }),
   }),
 });
 
-export const { useGetAnimeQuery } = animeApi;
+export const { useGetAnimeQuery, useGetAnimeByIdQuery } = animeApi;
