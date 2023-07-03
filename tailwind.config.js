809de3d1dba64b0a6bf76e62aba848d2c21d5e76
Yukine213 <kondratenkov1109@gmail.com>
2023-07-03 11:35:08 +0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#d71950",
+        grey: "#3d3c3d",
+        purple: "#a851ff",
+        pink: "#da5ea3",
+      },
+    },
   },
   plugins: [],
-}
-
+};
