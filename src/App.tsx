ae25b48dbc67784677c40fe1e87d500320dffb74
Yukine213 <kondratenkov1109@gmail.com>
2023-07-03 18:36:55 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import PopularPage from "./pages/PopularPage";
+import AnimeDetails from "./pages/AnimeDetails";
 
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/anime/all/popular" element={<PopularPage />} />
+        <Route path="/anime/:id" element={<AnimeDetails />} />
+      </Routes>
+    </BrowserRouter>
+
+    // TODO a loader for 4 sec
+  );
 }
 
 export default App;
