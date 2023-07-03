73c35fb5638b626ead6e000a67db8cb9b122988b
Yukine213 <kondratenkov1109@gmail.com>
2023-07-03 11:41:52 +0300

    remove api request for now and add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import { useGetAnimeQuery } from "./services/anime";
+import Header from "./components/Header";
 
 function App() {
-  const { data, isLoading, isError } = useGetAnimeQuery({
-    page: 1,
-    filter: "bypopularity",
-  });
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (isError) {
-    return <p>Error occurred while fetching data.</p>;
-  }
-
-  if (!data) {
-    return null; // or render a loading state
-  }
-
-  return (
-    <>
-      {data &&
-        data.map((anime) => (
-          <img
-            key={anime.mal_id}
-            src={anime.images.large_image_url}
-            alt="Anime"
-          />
-        ))}
-    </>
-  );
+  return <Header />;
 }
 
 export default App;
