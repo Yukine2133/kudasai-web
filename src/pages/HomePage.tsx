39395ba13a062582d34944c1cdcee3573e516925
Yukine213 <kondratenkov1109@gmail.com>
2023-07-03 18:38:46 +0300

    add popular section

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import PopularItem from "../components/PopularItem";
+
+const HomePage = () => {
+  return (
+    <main className="mt-10">
+      <PopularItem />
+    </main>
+  );
+};
+
+export default HomePage;
