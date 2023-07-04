f05b239130ed9fd8650fd45963ce29b9942e0e12
Yukine213 <kondratenkov1109@gmail.com>
2023-07-04 12:24:16 +0300

    add ongoing item

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import OngoingItem from "../components/OngoingItem";
 import PopularItem from "../components/PopularItem";
 
 const HomePage = () => {
   return (
     <main className="mt-10">
       <PopularItem />
+      <OngoingItem />
     </main>
   );
 };
